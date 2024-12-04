6854cc1780756cdf885b0b0c7907c2ecafa7c6b6
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2024-12-04 15:16:14 -0500

    lil cleanup

--- 2024/3/main.ts
+++ 2024/3/main.ts
@@ -1,7 +1,6 @@
 function solvePart1(input: string): number {
   const regexpMul = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
   const matchesMul = [...input.matchAll(regexpMul)];
-  // console.log(matchesMul[0].index);
   return matchesMul.reduce((acc, curr) => {
     const result = getMultipliedDigits(curr[0]);
     acc = acc + result;
@@ -18,10 +17,11 @@ function solvePart2(input: string): number {
   const regexpMul = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
   const regexpDo = /do\(\)/g;
   const regexpDont = /don't\(\)/g;
-  const matchesMul = [...input.matchAll(regexpMul)];
-  const matchesDo = [...input.matchAll(regexpDo)];
-  const matchesDont = [...input.matchAll(regexpDont)];
-  const allMatches = [...matchesMul, ...matchesDo, ...matchesDont];
+  const allMatches = [
+    ...input.matchAll(regexpMul),
+    ...input.matchAll(regexpDo),
+    ...input.matchAll(regexpDont),
+  ];
   allMatches.sort((a, b) => a.index - b.index);
   let shouldMultiply = true;
   return allMatches.reduce((acc, curr) => {
@@ -40,9 +40,8 @@ function solvePart2(input: string): number {
   }, 0);
 }
 
-const input1 = await Deno.readTextFile('./input.txt');
-const input2 = await Deno.readTextFile('./input.txt');
-const part1Result = solvePart1(input1);
-const part2Result = solvePart2(input2);
+const input = await Deno.readTextFile('./input.txt');
+const part1Result = solvePart1(input);
+const part2Result = solvePart2(input);
 console.log(part1Result);
 console.log(part2Result);
